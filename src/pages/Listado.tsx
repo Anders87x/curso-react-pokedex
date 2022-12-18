6162feea1bb031089d39d514f5405924b13e72de
AnderCode <davisanderson87@gmail.com>
2022-12-18 15:58:52 -0500

    18/12/22 Correcion build

--- src/pages/Listado.tsx
+++ src/pages/Listado.tsx
@@ -1,4 +1,4 @@
-import react,{useEffect,useState} from "react";
+import {useEffect,useState} from "react";
 /* TODO: Importar librerias de bootstrap */
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
